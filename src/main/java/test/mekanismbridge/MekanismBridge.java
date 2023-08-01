c20dcff1a1904f7899cc191bf9440d87aa32bf89
kingofspace <gageharoldruben@gmail.com>
2023-07-31 22:11:25 -0700

    Fixed bug that was brought to my attention in SolvedTicket#001

--- src/main/java/test/mekanismbridge/MekanismBridge.java
+++ src/main/java/test/mekanismbridge/MekanismBridge.java
@@ -24,6 +24,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.eventbus.api.Event.HasResult;
+import net.minecraftforge.eventbus.api.Event.Result;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
